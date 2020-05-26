48df6f7a2f89535fa444e81f4ef8f6e0c93fa4b1
Jaylan <jaylanwienckowski@gmail.com>
2020-05-26 11:41:23 -0500

    displaying 5-day forecast

--- Assets/script.js
+++ Assets/script.js
@@ -82,7 +82,38 @@ function getCurrentForecast () {
   }).then(function (response){
 
     console.log(response)
+    // Clearing the 5 day display of city before 
+    $('#forecast').empty();
+
     let results = response.list;
     console.log(results)
+    
+    for (let i = 0; i < results.length; i++) {
+      let day = Number(results[i].dt_txt.split('-')[2].split(' ')[0]);
+      console.log(day);
+
+      if(results[i].dt_txt.indexOf("12:00:00") !== -1){
+        
+    // Converting the temp from celsius to farenheit
+        let temp = (results[i].main.temp - 273.15) * 1.80 + 32;
+        let tempF = Math.floor(temp);
+
+    // Creating elements for each day  
+        const card = $("<div>").addClass("card col-md-2 ml-4 bg-primary text-white");
+        const cardBody = $("<div>").addClass("card-body p-3 forecastBody")
+        const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+        const temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + tempF + " °F");
+        const humidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + results[i].main.humidity + "%");
+
+    // Setting the icon to the image element
+        const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results[i].weather[0].icon + ".png")
+
+        cardBody.append(cityDate, image, temperature, humidity);
+        card.append(cardBody);
+        $("#forecast").append(card);
+
+      }
+    }
   });
+
 }
